f872145be8cd0539b055b4826fcf3a2f245c1c53
Rutger Kirkels <rutger@kirkels.nl>
2018-02-12 15:50:38 +0100

    Added Payment class

---
+++ src/Models/Payment.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace rutgerkirkels\ShopConnectors\Models;
+
+/**
+ * Class Payment
+ * @package rutgerkirkels\ShopConnectors\Models
+ *
+ * @author Rutger Kirkels <rutger@kirkels.nl>
+ */
+class Payment
+{
+    /**
+     * @var array
+     */
+    protected $statusses = ['paid', 'partially_paid', 'not_paid'];
+
+    /**
+     * @var string
+     */
+    protected $status;
+
+    /**
+     * @var
+     */
+    protected $type;
+
+    /**
+     * @return string
+     */
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param string $status
+     * @throws \Exception
+     */
+    public function setStatus(string $status): void
+    {
+        if (!in_array($status, $this->statusses)) {
+            throw new \Exception($status . ' is not a valid status');
+        }
+        $this->status = $status;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param mixed $type
+     */
+    public function setType($type): void
+    {
+        $this->type = $type;
+    }
+
+
+}
