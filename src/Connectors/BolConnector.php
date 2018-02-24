5142e4aab5651b24338a74d242e4a297f95e3055
Rutger Kirkels <rutger@kirkels.nl>
2018-02-24 22:38:17 +0100

    Bol.com Connector ready

--- src/Connectors/BolConnector.php
+++ src/Connectors/BolConnector.php
@@ -2,17 +2,34 @@
 
 namespace rutgerkirkels\ShopConnectors\Connectors;
 
-
 use GuzzleHttp\Client;
 use rutgerkirkels\ShopConnectors\Entities\Credentials\CredentialsInterface;
+use rutgerkirkels\ShopConnectors\Models\Customer;
 use rutgerkirkels\ShopConnectors\Models\DateRange;
+use rutgerkirkels\ShopConnectors\Models\DeliveryAddress;
+use rutgerkirkels\ShopConnectors\Models\InvoiceAddress;
+use rutgerkirkels\ShopConnectors\Models\Item;
+use rutgerkirkels\ShopConnectors\Models\Order;
+use rutgerkirkels\ShopConnectors\Models\OrderLine;
 
+/**
+ * Class BolConnector
+ * @package rutgerkirkels\ShopConnectors\Connectors
+ * @author Rutger Kirkels <rutger@kirkels.nl>
+ */
 class BolConnector extends AbstractConnector implements ConnectorInterface
 {
-
+    /**
+     * @var Client
+     */
     protected $webservice;
 
-    public function __construct(string $host = null, CredentialsInterface $credentials = null)
+    /**
+     * BolConnector constructor.
+     * @param string $host
+     * @param CredentialsInterface $credentials
+     */
+    public function __construct(string $host, CredentialsInterface $credentials)
     {
         parent::__construct($host, $credentials);
         $this->webservice = new Client([
@@ -20,29 +37,118 @@ class BolConnector extends AbstractConnector implements ConnectorInterface
         ]);
     }
 
+    /**
+     * @param DateRange $dateRange
+     * @return array
+     */
     public function getOrdersByOrderDate(DateRange $dateRange)
     {
         $headers = $this->getBolHeaders('GET', '/services/rest/orders/v2');
 
         $response = $this->webservice->request('GET', 'services/rest/orders/v2', [
-            'headers' => $headers
+            'headers' => $headers,
+            'query' => [
+                'fulfilment-method' => 'FBB'
+            ]
         ]);
 
-        var_dump($response->getBody());die;
+        $response = simplexml_load_string((string) $response->getBody());
+
+        $orders = [];
+        foreach ($response->Order as $bolOrder) {
+            $order = new Order();
+            $order->setDate($this->getTimestamp($bolOrder->DateTimeCustomer));
+            $order->setCustomer($this->getCustomer($bolOrder));
+            $order->setInvoiceAddress($this->getAddress($bolOrder->CustomerDetails->BillingDetails, InvoiceAddress::class));
+            $order->setDeliveryAddress($this->getAddress($bolOrder->CustomerDetails->ShipmentDetails, DeliveryAddress::class));
+            $order->setOrderLines($this->getOrderlines($bolOrder->OrderItems->OrderItem));
+            $order->setExternalData($this->getExternalData($bolOrder));
+            $orders[] = $order;
+        }
+
+        return $orders;
     }
 
+    /**
+     * @param \SimpleXMLElement $bolOrder
+     * @return Customer
+     */
+    protected function getCustomer(\SimpleXMLElement $bolOrder)
+    {
+        $customer = new Customer();
+        $customer->setFirstName($bolOrder->CustomerDetails->BillingDetails->Firstname);
+        $customer->setLastName($bolOrder->CustomerDetails->BillingDetails->Surname);
+        $customer->setEmail($bolOrder->CustomerDetails->BillingDetails->Email);
+
+        return $customer;
+    }
+
+    /**
+     * @param \SimpleXMLElement $data
+     * @param string $type
+     * @return mixed
+     */
+    protected function getAddress(\SimpleXMLElement $data, string $type)
+    {
+        $address = new $type;
+        $address->setAddress($data->Streetname . ' ' . $data->Housenumber);
+        $address->setPostalCode($data->ZipCode);
+        $address->setCity($data->City);
+        $address->setCountryIso2($data->CountryCode);
+
+        return $address;
+    }
+
+    /**
+     * @param \SimpleXMLElement $bolOrderlines
+     * @return array
+     */
+    protected function getOrderlines(\SimpleXMLElement $bolOrderlines)
+    {
+        $orderlines = [];
+        foreach ($bolOrderlines as $bolOrderline) {
+            $item = new Item();
+            $item->setName($bolOrderline->Title);
+            $item->setPriceWithTax(floatval($bolOrderline->OfferPrice));
+            $item->setEan13(intval($bolOrderline->EAN));
+            $item->setSku($bolOrderline->OfferReference);
+
+            $orderlines[] = new OrderLine($item, floatval($bolOrderline->Quantity));
+        }
+
+        return $orderlines;
+    }
+
+    /**
+     * @param \SimpleXMLElement $bolOrder
+     * @return Order\ExternalData
+     */
+    protected function getExternalData(\SimpleXMLElement $bolOrder)
+    {
+        $externalData = new Order\ExternalData();
+        $externalData->setOrderId(intval($bolOrder->OrderId));
+        $externalData->setJson(json_encode($bolOrder));
+
+        return $externalData;
+    }
+
+    /**
+     * @param string $method
+     * @param string $uri
+     * @return mixed
+     */
     protected function getBolHeaders(string $method, string $uri)
     {
-        $timestamp = new \DateTime();
         $signature = strtoupper($method) . PHP_EOL . PHP_EOL;
         $signature .= 'application/xml' . PHP_EOL;
-        $signature .= $timestamp->format('r') . PHP_EOL;
-        $signature .= 'x-bol-date:' . $timestamp->format('r') . PHP_EOL;
+        $signature .= gmdate('D, d M Y H:i:s T') . PHP_EOL;
+        $signature .= 'x-bol-date:' . gmdate('D, d M Y H:i:s T') . PHP_EOL;
         $signature .= $uri;
 
         $headers['X-BOL-Authorization'] = $this->credentials->getPublicKey() . ':' . base64_encode(hash_hmac('SHA256', $signature, $this->credentials->getPrivateKey(), true));
-        $headers['X-BOL-Date'] = $timestamp->format('r');
+        $headers['X-BOL-Date'] = gmdate('D, d M Y H:i:s T');
         $headers['Content-type'] = 'application/xml';
+        $headers['Accept'] = 'application/vnd.orders-v2.1+xml';
 
         return $headers;
     }
